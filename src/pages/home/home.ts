9003645302a4fa3cc786c647b10f973d01d6e811
Robert Mayer <rwlmayer@gmail.com>
2017-12-08 09:21:37 +0100

    Created new service and created eventemitter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,45 +1,29 @@
+/// <reference path="../../../typings/index.d.ts" />
+
 import { Component } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 import { RestaurantPage } from '../restaurant/restaurant';
+import { RestaurantApiService } from '../../services/restaurantapi.service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [RestaurantApiService]
 })
 export class HomePage {
-restaurantUrl: string = 'https://api.integration.devtest.aws.scania.com/lunch/1.0/restaurants';
-  
   restaurants: any = null;
-  menu: any = null;
 
-  constructor(public navCtrl: NavController, private http: Http) {
-    this.callRestaurantEndpoint();
-  }
-
-  callRestaurantEndpoint() {
-    var token = 'ab3085af-d43a-34e8-9782-cf0c235e26d5';
-
-    let headers: Headers = new Headers();
-    headers.append('Authorization', 'Bearer ' + token);
-
-    let method = 'GET';
-    
-    let requestOptions: RequestOptions = new RequestOptions({
-      headers: headers,
-      method: method
-    });
-    
-
-    this.http.get(this.restaurantUrl, requestOptions).map(res => res.json()).subscribe(data => {
-      console.log(data);
-      this.restaurants = data;
-    });
+  constructor(public navCtrl: NavController, private http: Http, public restaurantApiService: RestaurantApiService) {
+    this.restaurantApiService.isAuthorized.subscribe(data => {
+      this.restaurantApiService.callRestaurantEndpoint().subscribe(data => {
+        this.restaurants = data;
+      });
+    });    
   }
 
   restaurantChosen(id, name) {
-    console.log(id, name);
     this.navCtrl.push(RestaurantPage, {
       restaurantId: id,
       name: name
---
+++ src/services/restaurantapi.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class RestaurantApiService {
+
+    private _restaurantsUrl: string = 'https://api.integration.devtest.aws.scania.com/lunch/1.0/restaurants';
+    private _tokenUrl: string = 'https://api.integration.devtest.aws.scania.com/token';
+    private _consumerKey: string = 'evE0u48rNAxxDw2Rl_YQHmKzQbEa';
+    private _consumerSecret: string = 'EVLWUbtbwi1zXLaMr3j3ixmvRgsa';
+
+    private _accessToken: string;
+
+    public isAuthorized: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    constructor(private http: Http) {
+        this.getAccessToken(this._consumerKey, this._consumerSecret);
+    }
+
+    getAccessToken(consumerKey: string, consumerSecret: string) {
+        var buffer = new Buffer(consumerKey + ":" + consumerSecret).toString('base64');
+    
+        let headers: Headers = new Headers();
+        headers.append('Authorization', 'Basic ' + buffer);
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        
+        var dataString = 'grant_type=client_credentials';
+    
+        let requestOptions: RequestOptions = new RequestOptions({
+          headers: headers,
+          method: 'POST',
+          body: dataString
+        });
+    
+        return this.http.post(this._tokenUrl, dataString, requestOptions).map(res => res.json()).subscribe(data => {
+            this._accessToken = data.access_token;
+
+            this.isAuthorized.emit(true);
+        });
+    }
+
+    callRestaurantEndpoint() {
+        let headers: Headers = new Headers();
+        headers.append('Authorization', 'Bearer ' + this._accessToken);
+    
+        let requestOptions: RequestOptions = new RequestOptions({
+            headers: headers,
+            method: 'GET'
+        });
+            
+        return this.http.get(this._restaurantsUrl, requestOptions).map(res => res.json());
+    }
+}
